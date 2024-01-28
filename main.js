0e20e012b782d85598036d494b76a2d73d179854
Git-Tchoupirouette <157950898+Git-Tchoupirouette@users.noreply.github.com>
2024-01-28 13:12:30 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
-function generateRandomNumber() {
-    var randomNumber = Math.floor(Math.random() * 100) + 1;
-    document.getElementById("randomNumberDisplay").innerText = "Le nombre aléatoire est : " + randomNumber;
-}
+document.getElementById('addOverlayBtn').addEventListener('click', function() {
+    let imgInput = document.getElementById('imgInput');
+    let canvas = document.getElementById('canvas');
+    let ctx = canvas.getContext('2d');
+
+    let img = new Image();
+    let file = imgInput.files[0];
+    let reader = new FileReader();
+
+    reader.onloadend = function() {
+        img.onload = function() {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img, 0, 0);
+            // Ajouter votre élément par-dessus l'image ici
+            // Par exemple : ctx.fillText('Mon texte', 50, 50);
+            document.getElementById('downloadLink').href = canvas.toDataURL("image/jpeg");
+        }
+        img.src = reader.result;
+    }
+    if (file) {
+        reader.readAsDataURL(file);
+    } else {
+        alert('Veuillez sélectionner une image.');
+    }
+});
