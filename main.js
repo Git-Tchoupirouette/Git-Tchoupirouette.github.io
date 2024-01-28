59265076c7fb30b178a28adb578eecdfa03e2377
Git-Tchoupirouette <157950898+Git-Tchoupirouette@users.noreply.github.com>
2024-01-28 22:58:32 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,25 +3,31 @@ document.getElementById('addOverlayBtn').addEventListener('click', function() {
     let canvas = document.getElementById('canvas');
     let ctx = canvas.getContext('2d');
 
-    let img = new Image();
-    let file = imgInput.files[0];
-    let reader = new FileReader();
+    if (imgInput.files.length !== 0) {
+        let imgFile = imgInput.files[0];
+        let img = new Image();
+        let reader = new FileReader();
 
-    reader.onloadend = function() {
-        img.onload = function() {
-            canvas.width = img.width;
-            canvas.height = img.height;
-            ctx.drawImage(img, 0, 0);
-            // Ajouter votre élément par-dessus l'image ici
-            // Par exemple : ctx.fillText('Mon texte', 50, 50);
-            ctx.fillText('Salut', 50, 50);
-            document.getElementById('downloadLink').href = canvas.toDataURL("image/jpeg");
+        reader.onload = function(e) {
+            img.src = e.target.result;
+            img.onload = function() {
+                if (img.width !== img.height) {
+                    alert('Veuillez télécharger une image carrée.');
+                    // Réinitialiser l'input pour que l'utilisateur puisse télécharger une nouvelle image
+                    imgInput.value = '';
+                } else {
+                    canvas.width = img.width;
+                    canvas.height = img.height;
+                    ctx.drawImage(img, 0, 0);
+                    // Ajouter votre élément par-dessus l'image ici
+                    // Par exemple : ctx.fillText('Mon texte', 50, 50);
+                    ctx.fillText('Salut', 50, 50);
+                    document.getElementById('downloadLink').href = canvas.toDataURL("image/jpeg");
+                }
+            };
         }
-        img.src = reader.result;
-    }
-    if (file) {
-        reader.readAsDataURL(file);
+        reader.readAsDataURL(imgFile);
     } else {
-        alert('Veuillez sélectionner une image.');
+        alert("Veuillez sélectionner une image");
     }
 });
