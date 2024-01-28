0ab4e41823b4d0ee5f1021100465ca266124991a
Git-Tchoupirouette <157950898+Git-Tchoupirouette@users.noreply.github.com>
2024-01-28 11:07:47 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
+function generateRandomNumber() {
+    // Générer un nombre aléatoire entre 1 et 100
+    var randomNumber = Math.floor(Math.random() * 100) + 1;
 
+    // Afficher le nombre aléatoire dans un paragraphe avec l'id "randomNumber"
+    document.getElementById("randomNumber").innerHTML = "Le nombre aléatoire est : " + randomNumber;
+}
