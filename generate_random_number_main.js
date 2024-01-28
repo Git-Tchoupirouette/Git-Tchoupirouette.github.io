6dc1a583077f804f7893f6e512701677eb6d42c7
Git-Tchoupirouette <157950898+Git-Tchoupirouette@users.noreply.github.com>
2024-01-28 13:17:34 +0100

    Delete generate_random_number_main.js

--- generate_random_number_main.js
+++
@@ -1,4 +0,0 @@
-function generateRandomNumber() {
-    var randomNumber = Math.floor(Math.random() * 100) + 1;
-    document.getElementById("randomNumberDisplay").innerText = "Le nombre aléatoire est : " + randomNumber;
-}
