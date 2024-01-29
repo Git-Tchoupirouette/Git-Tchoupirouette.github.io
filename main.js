7ae6e49701b9f9093651f4ed0bec29ac94f43997
Git-Tchoupirouette <157950898+Git-Tchoupirouette@users.noreply.github.com>
2024-01-29 01:01:16 +0100

    Update main.js

--- main.js
+++ main.js
@@ -11,8 +11,6 @@ document.getElementById('addOverlayBtn').addEventListener('click', function() {
         reader.onload = function(e) {
             img.src = e.target.result;
             img.onload = function() {
-                console.log(img.width, img.height);
-                
                 if (img.width !== img.height) {
                     alert('Veuillez télécharger une image carrée.');
                     // Réinitialiser l'input pour que l'utilisateur puisse télécharger une nouvelle image
@@ -21,10 +19,13 @@ document.getElementById('addOverlayBtn').addEventListener('click', function() {
                     canvas.width = img.width;
                     canvas.height = img.height;
                     ctx.drawImage(img, 0, 0);
-                    // Ajouter votre élément par-dessus l'image ici
-                    // Par exemple : ctx.fillText('Mon texte', 50, 50);
-                    ctx.fillText('Ptn de merde pk ça marche pas', 50, 50);
-                    document.getElementById('downloadLink').href = canvas.toDataURL("image/jpeg");
+
+                    let overlay = new Image();
+                    overlay.src = 'overlay.jpeg';
+                    overlay.onload = function() {
+                        ctx.drawImage(overlay, 0, 0, canvas.width, canvas.height);
+                        document.getElementById('downloadLink').href = canvas.toDataURL("image/jpeg");
+                    };
                 }
             };
         }
