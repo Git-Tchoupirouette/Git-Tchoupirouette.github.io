5d8312ba14934054d8081f68a6514f7c560134a8
Git-Tchoupirouette <157950898+Git-Tchoupirouette@users.noreply.github.com>
2024-01-29 01:39:43 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,34 @@
-document.getElementById('addOverlayBtn').addEventListener('click', function() {
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('addOverlayBtn').addEventListener('click', function() {
     let imgInput = document.getElementById('imgInput');
-    let canvas = document.getElementById('canvas');
-    let ctx = canvas.getContext('2d');
+    let userImage = document.getElementById('userImage');
 
     if (imgInput.files.length !== 0) {
-        let imgFile = imgInput.files[0];
-        let img = new Image();
-        let reader = new FileReader();
+      let imgFile = imgInput.files[0];
+      let reader = new FileReader();
 
-        reader.onload = function(e) {
-            let img = new Image();
-            img.src = e.target.result;
-            img.onload = function() {
-                if (img.width !== img.height) {
-                    alert('Veuillez télécharger une image carrée.');
-                    imgInput.value = '';
-                } else {
-                    let baseImage = new Image();
-                    baseImage.src = 'base_image.png'; // Spécifiez le nom de votre image de base
-                    baseImage.onload = function() {
-                        canvas.width = baseImage.width;
-                        canvas.height = baseImage.height;
-                        ctx.drawImage(baseImage, 0, 0);
-                        ctx.drawImage(img, 0, 0, baseImage.width, baseImage.height); // Superposer l'image de l'utilisateur à l'image de base avec les mêmes dimensions
-                        document.getElementById('downloadLink').href = canvas.toDataURL("image/png"); // Spécifiez le type d'image ici
-                    };
-                }
-            };
-        }
-        reader.readAsDataURL(imgFile);
+      reader.onload = function(e) {
+        userImage.src = e.target.result;
+      }
+      reader.readAsDataURL(imgFile);
     } else {
-        alert("Veuillez sélectionner une image");
+      alert("Veuillez sélectionner une image");
     }
+  });
+
+  document.getElementById('downloadLink').addEventListener('click', function() {
+    let canvas = document.createElement('canvas');
+    let baseImage = document.getElementById('baseImage');
+    let userImage = document.getElementById('userImage');
+
+    canvas.width = baseImage.width;
+    canvas.height = baseImage.height;
+    let context = canvas.getContext('2d');
+    context.drawImage(baseImage, 0, 0, baseImage.width, baseImage.height);
+    context.globalAlpha = 0.5; // Réglez l'opacité selon vos besoins
+    context.drawImage(userImage, 0, 0, baseImage.width, baseImage.height);
+
+    let downloadLink = document.getElementById('downloadLink');
+    downloadLink.href = canvas.toDataURL('image/png');
+  });
 });
